9fdeeaebcd9fc4ab46fa071e76cb35bd4fdb30c4
Em3L <martink_lopez@yahoo.com>
2023-08-20 16:12:28 +1000

    // update a category by its `id` value router.put('/:id', (req, res) => {   try {     const updatedCategory = await Category.update(req.body, {       where: { id: req.params.id },     });
        if (updatedCategory[0] === 0) {
          res.status(404).json({ message: 'Category not found'});
          return;
        }
    
        res.status(200).json({ message: 'Category updated successfully' });
      } catch (err) {
        console.error(err);
        res.status(500).json(err);
      }
    });

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -50,6 +50,16 @@ router.put('/:id', (req, res) => {
     const updatedCategory = await Category.update(req.body, {
       where: { id: req.params.id },
     });
+
+    if (updatedCategory[0] === 0) {
+      res.status(404).json({ message: 'Category not found'});
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
   }
 });
 
